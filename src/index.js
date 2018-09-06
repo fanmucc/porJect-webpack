039dfacb07a579c450ee7d67c06c0f2d85613622
fanmucc <fanmucc@yeah.net>
2018-09-06 22:03:17 +0800

    demo2

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import _ from 'lodash'
 import j from 'jquery'
+import foo from './foo'
 
 function component() {
     // let element = document.createElement('div')
     // element.innerHTML = _.join(['Helloaaaaa','webpack'],' ')
     // return element
 
-    var element = j('<div><div>')
+    var element = j('<div></div>')
+    element.html(_.join(['Hello111','webpack'],' '))
+    return element.get(0)
+    
+}
+function aaa() {
+    // let element = document.createElement('div')
+    // element.innerHTML = _.join(['Helloaaaaa','webpack'],' ')
+    // return element
+
+    var element = j('<div></div>')
     element.html(_.join(['Hello','webpack'],' '))
     return element.get(0)
+    
 }
 document.body.appendChild(component())
+document.body.appendChild(aaa())
+console.log('1')
+console.log(foo)
+console.log(foo())
