3583c659ff139246943e674356aaa85dec2d5de8
fanmucc <fanmucc@yeah.net>
2018-09-06 22:08:49 +0800

    demo2

--- src/index.js
+++ src/index.js
@@ -12,18 +12,9 @@ function component() {
     return element.get(0)
     
 }
-function aaa() {
-    // let element = document.createElement('div')
-    // element.innerHTML = _.join(['Helloaaaaa','webpack'],' ')
-    // return element
 
-    var element = j('<div></div>')
-    element.html(_.join(['Hello','webpack'],' '))
-    return element.get(0)
-    
-}
 document.body.appendChild(component())
-document.body.appendChild(aaa())
+
 console.log('1')
 console.log(foo)
 console.log(foo())
