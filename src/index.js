26fef9b2f889ec519aed29cdc6be6814716c6e2f
fanmucc <fanmucc@yeah.net>
2018-09-07 22:49:51 +0800

    update

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+ReactDOM.render(
+    <h1>Hello, world!</h1>,
+    document.getElementById('root')
+  );
+  ReactDOM.render(
+    <h1>Hi, world!</h1>,
+    document.getElementById('root1')
+  );
+// ReactDOM.render(<App />, document.getElementById('root'));  将app.js的文件部署到root里面
+// registerServiceWorker();
+
+
